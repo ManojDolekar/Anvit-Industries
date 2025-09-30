19c69dd5585fad3bb545c67c9603c555a23de00e
ManojDolekar <manojdolekar9@gmail.com>
2025-09-30 16:11:54 +0530

    update / for url

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
+  <BrowserRouter basename="/Anvit-Industries">
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
